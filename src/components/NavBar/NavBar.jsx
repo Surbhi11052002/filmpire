347bd10ec5c0532191b99eb5b424b901c4cafd0f
Surbhi <94106436+Surbhi11052002@users.noreply.github.com>
2023-04-15 00:47:58 +0530

    created Navbar component

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,77 @@
 import React from "react";
+import {
+  AppBar,
+  IconButton,
+  Toolbar,
+  Drawer,
+  Button,
+  Avatar,
+  useMediaQuery,
+} from "@mui/material";
+import {
+  Menu,
+  AccountCircle,
+  Brightness4,
+  Brightness7,
+} from "@mui/icons-material";
+
+import { useTheme } from "@mui/material/styles";
+
+import { Link } from "react-router-dom"; // Link allows us to switch between different pages
+
+import useStyles from "./styles";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  const classes = useStyles();
+  const isMobile = useMediaQuery("(max-width:600px)"); // anything greater than 600px will not be mobile
+  const theme = useTheme();
+  const isAuthenticated = "true";
+  return (
+    <>
+      <AppBar position="fixed">
+        <Toolbar className={classes.toolbar}>
+          {isMobile && (
+            <IconButton
+              color="inherit"
+              edge="start"
+              style={{ outline: "none" }}
+              onClick={() => {}}
+              className={classes.menuButton}
+            >
+              <Menu />
+            </IconButton>
+          )}
+          <IconButton color="inherit" sx={{ ml: 1 }} onClick={() => {}}>
+            {theme.palette.mode === "dark" ? <Brightness7 /> : <Brightness4 />}
+          </IconButton>
+          {!isMobile && "Search.."}
+          <div>
+            {!isAuthenticated ? (
+              <Button color="inherit" onClick={() => {}}>
+                Login &nbsp; <AccountCircle />
+              </Button>
+            ) : (
+              <Button
+                color="inherit"
+                component={Link}
+                to={`/profile/:id`}
+                className={classes.linkButton}
+                onClick={() => {}}
+              >
+                {!isMobile && <>My Movies &nbsp; </>}
+                <Avatar
+                  style={{ width: 30, height: 30 }}
+                  alt="Profile"
+                  src="https://th.bing.com/th/id/OIP.STkwDNjAVAtBdbzv1UMnQwHaHa?pid=ImgDet&rs=1"
+                />
+              </Button>
+            )}
+          </div>
+          {isMobile && "Search..."}
+        </Toolbar>
+      </AppBar>
+    </>
+  );
 };
 
 export default NavBar;
