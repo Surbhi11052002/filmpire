4254b1c3742c0aca96e649e7e4d163336ef355c0
Surbhi <94106436+Surbhi11052002@users.noreply.github.com>
2023-05-31 18:50:43 +0530

    redux-setup

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import {
+  Box,
+  CircularProgress,
+  useMediaQuery,
+  Typography,
+} from "@mui/material";
+import { useSelector } from "react-redux";
 
-const Movies = () => {
+import { useGetMoviesQuery } from "../../services/TMDB";
+
+// const Movies = () => {
+//   const { data } = useGetMoviesQuery();
+//   console.log(data);
+//   return <div>Movies</div>;
+// };
+function Movies() {
+  const { data } = useGetMoviesQuery(); //fetching data from an API
+  console.log(data);
+
   return <div>Movies</div>;
-};
+}
 
 export default Movies;
--- src/index.js
+++ src/index.js
@@ -2,15 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-
+import { Provider } from "react-redux";
 import App from "./components/App";
+
+import store from "./app/store";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 const theme = createTheme({});
 
-ReactDOM.render(
-  <ThemeProvider theme={theme}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ThemeProvider>,
-  document.getElementById("root")
+root.render(
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
+  </Provider>
 );
